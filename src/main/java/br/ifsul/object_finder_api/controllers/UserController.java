622692ac21a88e4f607af835278789277df62914
fernandobarrosd <fdebarros0910-2004@hotmail.com>
2023-11-22 12:19:50 -0300

    Other commit

--- src/main/java/br/ifsul/object_finder_api/controllers/UserController.java
+++ src/main/java/br/ifsul/object_finder_api/controllers/UserController.java
@@ -1,15 +1,22 @@
 package br.ifsul.object_finder_api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.ifsul.object_finder_api.dtos.LoginDTO;
 import br.ifsul.object_finder_api.dtos.LostObjectDTO;
 import br.ifsul.object_finder_api.entities.User;
 import br.ifsul.object_finder_api.services.UserService;
 import jakarta.validation.Valid;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
